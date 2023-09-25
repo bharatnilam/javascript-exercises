1661985caae741408a5eec7757a760f1631a8f26
Bharat Nilam <46712950+bharatnilam@users.noreply.github.com>
2023-09-26 02:11:26 +0530

    Complete 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n<0)
+        return "OOPS"
+    n = parseInt(n);
+    if (n === 0 || n === 1)
+        return n;
+    
+    let prev1 = 0;
+    let prev2 = 1
+    let curr = 0;
 
+    for (i=2;i<=n;i++){
+        curr = prev1 + prev2;
+        prev1=prev2;
+        prev2=curr;
+    }
+    return curr;
 };
 
 // Do not edit below this line
