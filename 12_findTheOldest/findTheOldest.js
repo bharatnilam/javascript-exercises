9ac40d12307ba2d62a23d3d1961ddf062136bbe9
Bharat Nilam <46712950+bharatnilam@users.noreply.github.com>
2023-09-26 02:35:28 +0530

    Complete 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    if (people.length === 0) {
+        return null;
+    }
 
+    const currentYear = new Date().getFullYear();
+
+    let oldestPerson = people[0]; 
+
+    const calculateAge = (person) => {
+        if (person.yearOfDeath) {
+            return person.yearOfDeath - person.yearOfBirth;
+        } else {
+            return currentYear - person.yearOfBirth;
+        }
+    };
+
+    for (let i = 1; i < people.length; i++) {
+        const ageOldest = calculateAge(oldestPerson);
+        const ageCurrent = calculateAge(people[i]);
+
+        if (ageCurrent > ageOldest) {
+            oldestPerson = people[i];
+        }
+    }
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
