bc4d906c585366e7d874ff96fc20fe50c756e266
Bharat Nilam <46712950+bharatnilam@users.noreply.github.com>
2023-09-26 02:02:58 +0530

    Complete 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    const clean = text.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    return clean === clean.split('').reverse().join(''); 
 };
 
 // Do not edit below this line
