fd18691144c21d9e06dfc576bb9d0b038da5bdf3
Bharat Nilam <46712950+bharatnilam@users.noreply.github.com>
2023-09-26 01:52:59 +0530

    Complete 08_calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return parseInt(a) + parseInt(b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return parseInt(a) - parseInt(b);
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	let summ = 0;
+  nums.forEach(num => {
+    summ += num;
+  });
+  return summ;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let mult = 1;
+  nums.forEach(num => {
+    mult *= num;
+  });
+  return mult;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let res = 1, i;
+  for (i=2;i<=a;i++)
+    res *= i;
+  return res;
 };
 
 // Do not edit below this line
